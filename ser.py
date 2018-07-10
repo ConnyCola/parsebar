0648b1b07a7c6b5d642497b40d375589e2f762b1
ConnyCola <MatthiasWagner@zoho.com>
2018-07-10 22:07:25 +0200

    changed gui to be completly dynamic

--- ser.py
+++ ser.py
@@ -1,11 +1,11 @@
 import sys
 from serial import *
-from Tkinter import *
+from tkinter import *
 from tkinter import ttk
 import matplotlib
 # matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import numpy as np
+#import numpy as np
 import time
 
 arg = sys.argv[2:]
@@ -22,31 +22,39 @@ root.wm_title("Reading Serial")
 lab_array = []
 progress_array = []
 search_array = []
-plot_array = range(500)
-value_array = range(500)
+num_array = range(500)
+va = range(500)
+value_array = []
 
+plot_array = []
 
-print plot_array
-print value_array
+# print arg
 
 for i in range(len(arg) / 2):
-    print "generate " + str(arg[i * 2])
-    l = Label(root, text="", font="Helvetica 16")
+    print("generate " + str(arg[i * 2]))
+    l = Label(root, text="", font="Helvetica 16", width=13)
     pb = ttk.Progressbar(root, orient='horizontal', mode='determinate', length=500)
     pb["maximum"] = int(arg[i * 2 + 1])
     lab_array.append(l)
+
+    value_array.append(va)
+
     progress_array.append(pb)
     search_array.append(str(arg[i * 2]))
+    # put widgets in grid
     l.grid(row=i, column=0)
     pb.grid(row=i, column=1)
+
+    plt.subplot((100 * len(arg) / 2 + 100) + 11 + i)
+    plt.axis([0, 500, 0, int(arg[i * 2 + 1])])
+    p, = plt.plot([], [])
+    plot_array.append(p)
 
 
 # make our own buffer
 # useful for parsing commands
 # Serial.readline seems unreliable at times too
 serBuffer = ""
-plt.axis([0, 500, 0, 300])
-h1, = plt.plot([], [])
 
 
 def readSerial():
@@ -60,9 +68,7 @@ def readSerial():
 
         # get the buffer from outside of this function
         global serBuffer
-        global plot_array
         global value_array
-        global h1
         # check if character is a delimeter
         if c == '\r':
             c = ''  # don't want returns. chuck it
@@ -77,13 +83,12 @@ def readSerial():
                         lab_array[i].configure(text=serBuffer)
                         progress_array[i]["value"] = v
 
-                        if i == 0:
-                            v = int(float(v))
-                            value_array = value_array[1:]
-                            value_array.append(v)
+                        v = int(float(v))
+                        value_array[i] = value_array[i][1:]
+                        value_array[i].append(v)
 
                     except:
-                        print "Error"
+                        print("Error")
                         pass
 
             serBuffer = ""  # empty the buffer
@@ -95,21 +100,22 @@ def readSerial():
 
 
 def updatePlot():
-    global plot_array
+    global num_array
     global value_array
-    global h1
+    global plot_array
 
-    h1.set_ydata(value_array)
-    h1.set_xdata(plot_array)
+    for i, p in enumerate(plot_array):
+        p.set_ydata(value_array[i])
+        p.set_xdata(num_array)
+
     plt.pause(0.001)
-    time.sleep(0.001)
-    root.after(33, updatePlot)  # check serial again soon
+    root.after(30, updatePlot)  # check serial again soon
 
 
 # after initializing serial, an arduino may need a bit of time to reset
 
 root.after(100, readSerial)
-root.after(100, updatePlot)
+root.after(200, updatePlot)
 root.geometry("650x" + str(root.winfo_reqheight()))
 
 root.mainloop()
